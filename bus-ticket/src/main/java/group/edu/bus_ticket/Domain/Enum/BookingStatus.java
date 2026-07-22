87278454dfadd3e71b6a705b73b48e7081fca0a7
Omachi100 <Omachi100>
2026-07-22 20:04:51 +0700

    Add status enums for staff booking flow

---
+++ bus-ticket/src/main/java/group/edu/bus_ticket/Domain/Enum/BookingStatus.java
@@ -0,0 +1,10 @@
+package group.edu.bus_ticket.Domain.Enum;
+
+public enum BookingStatus {
+    PENDING_PAYMENT,
+    CONFIRMED,
+    CANCELLED,
+    EXPIRED,
+    REFUND_PENDING,
+    REFUNDED
+}
--- bus-ticket/src/main/java/group/edu/bus_ticket/Domain/Enum/PaymentMethod.java
+++ bus-ticket/src/main/java/group/edu/bus_ticket/Domain/Enum/PaymentMethod.java
@@ -2,5 +2,6 @@ package group.edu.bus_ticket.Domain.Enum;
 
 public enum PaymentMethod {
     COD,
-    CASH
+    CASH,
+    BANK_TRANSFER
 }
